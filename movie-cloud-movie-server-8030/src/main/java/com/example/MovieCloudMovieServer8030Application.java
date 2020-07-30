9d117ed2ae6556fe59e4c4e4b51a3d9b884b9936
zhen <810911851@qq.com>
2020-07-30 21:43:03 +0800

    2020-07-30 新建movie部分

---
+++ movie-cloud-movie-server-8030/src/main/java/com/example/MovieCloudMovieServer8030Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCloudMovieServer8030Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCloudMovieServer8030Application.class, args);
+    }
+
+}
---
+++ movie-cloud-movie-server-8030/src/test/java/com/example/MovieCloudMovieServer8030ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCloudMovieServer8030ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
